04876a3570ea165c337c2f323f8e9da6dd26b3ad
igor-chazov <igor-chazov@yandex.ru>
2021-08-31 01:40:24 +0500

    Доработано ДЗ

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 import GameSavingLoader from './gamesavingloader';
+import GameSaving from './gamesaving';
 
-GameSavingLoader.load().then((saving) => JSON.parse(saving), (error) => {
+GameSavingLoader.load().then((saving) => {
+  const { id, created, userInfo } = JSON.parse(saving);
+  return new GameSaving(id, created, userInfo.id, userInfo.name, userInfo.level, userInfo.points);
+}, (error) => {
   throw error;
 });
